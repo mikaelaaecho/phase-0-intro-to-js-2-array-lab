1bfaaba0f0aeac87b5a0a855237276f11dc1987c
mikaela echo <mikaelaaecho@gmail.com>
2023-08-21 16:18:20 -0600

    compltete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  function appendCat(name) {
+    const newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+  }
+  function prependCat(name) {
+    return [name, ...cats]
+  }
+
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+
+  function removeFirstCat() {
+    const [_, ...remainingCats] = cats;
+    return remainingCats;
+  }
